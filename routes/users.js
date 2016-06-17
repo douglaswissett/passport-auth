7a88ec975eba8b355892159ccb76d962d1d1576b
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-06-17 12:52:39 +0200

    db dump endpoint geo index conversion

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@ var Db_dump = require('../models/db_dump');
 module.exports = function(User, LocationSchema) {
   /* GET users listing. */
   router.get('/', function(req, res) {
-
     User.findAll(function(err, users){
       res.render('show_users.jade', { data: users, title: 'Users', description: 'kompas_test userbase' });
     });
@@ -21,7 +20,6 @@ module.exports = function(User, LocationSchema) {
     
     User.findOne({ 'username': req.user.username }, function (err, user) {
       if (err) throw err;
-
       LocationSchema.find({'geo': {$near: [ user.coordinates[0] , user.coordinates[1] ], $maxDistance: 1000/6371}}, 
       function(err, locations) {
         res.render('show_map.jade',{ data: locations, center: { lat: user.coordinates[1] , lng: user.coordinates[0] } });
@@ -30,45 +28,43 @@ module.exports = function(User, LocationSchema) {
   });
 
   router.post('/getLocation', function(req, res) {
-
     // Update Users Location
     User.update({username: req.user.username},{
       coordinates: [req.body.lng, req.body.lat]
     }, function(err) {
       console.error(err);
     });
-
-
-    // convert dump data into geoindexable data
-    Db_dump.find({},function(err, venues){
-      venues.forEach(function(venue) {
-        console.log(venue.name);
-        // Save location data
-        var locationModel     = new LocationSchema(); 
-        locationModel.name = venue.name;
-        locationModel.location = venue.location;
-        locationModel.city = venue.city;
-        locationModel.geo    = [venue.longitude, venue.latitude];
-        locationModel.latitude = venue.latitude;
-        locationModel.longitude = venue.longitude;
-        locationModel.related_ids = venue.related_ids;
-        locationModel.description = venue.description;
-        locationModel.meta_tags = venue.meta_tags;
-        locationModel.photo_link = venue.photo_link;
-        locationModel.save();
-      });
-    });
-
-    
     // Get location data
     User.findOne({ 'username': req.user.username }, function (err, user) {
       if (err) throw err;
-
       LocationSchema.find({'geo': {$near: [ user.coordinates[0] , user.coordinates[1] ], $maxDistance: 1000/6371}}, 
       function(err, locations) {
         res.json(locations);
       });
     })
+  });
+
+  // convert dump data into geoindexable data
+  router.get('/dump', function(req ,res) {
+    Db_dump.find({},function(err, places){
+      places.forEach(function(place) {
+        // Save location data
+        var locationModel     = new LocationSchema(); 
+        locationModel.name = place.name;
+        locationModel.location = place.location;
+        locationModel.city = place.city;
+        locationModel.geo    = [place.longitude, place.latitude];
+        locationModel.latitude = place.latitude;
+        locationModel.longitude = place.longitude;
+        locationModel.related_ids = place.related_ids;
+        locationModel.description = place.description;
+        locationModel.meta_tags = place.meta_tags;
+        locationModel.photo_link = place.photo_link;
+        locationModel.save();
+      });
+    });
+    var html = '<html>Dump data seeded, <a href="/">go back</a></html>'
+    res.send(html);
   });
 
   return router;
