373090ea78afa0e97c572d73e8d3d4f487f3cddb
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-06-22 10:57:13 +0200

    configs

--- db/db.js
+++ db/db.js
@@ -1,3 +1,10 @@
-module.exports = {
-  'url' : `mongodb://localhost/${process.env.DB_NAME}`
+if(process.env.ENVIRONMENT === 'production') {
+  module.exports = {
+    'url' : process.env.MONGODB_URI
+  }
+} else {
+  module.exports = {
+    'url' : `mongodb://localhost/${process.env.DB_NAME}`
+  }
 }
+
