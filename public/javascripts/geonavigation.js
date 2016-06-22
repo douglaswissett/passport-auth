20103f13e056f974859483da30f81db9fa20f090
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-06-22 14:30:39 +0200

    removed geonavigator BUGGY

--- public/javascripts/geonavigation.js
+++ public/javascripts/geonavigation.js
@@ -3,51 +3,45 @@ $( document ).ready(function(){
   // Google map
   var map;
 
-
-
-
-
-  function geo_success(position) {
-    console.log( position.coords);
-    $.ajax({
-      url: '/api/v1/users/getLocation',
-      type: 'POST',
-      data: { 
-        lng: position.coords.longitude,
-        lat: position.coords.latitude
-      }
-    })
-    .done(function(data) {
-      data.forEach(function(location) {
-
-        var marker = new google.maps.Marker({
-          position: {lat: location.geo[1], lng: location.geo[0]},
-          map: map
-        });
-        attachMarkerMessage(marker, location.name);
-
-        console.log(location);
-        var content = $(`<a href="">${location.name}</a>`)
-        var div = $('<div>').append(content)
-        var li = $('<li>').append(div)
-
-        $('.places-ul').append(li)
-      })
-    })
-
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: position.coords.latitude, lng: position.coords.longitude},
-        zoom: 14
-      });
+  $.ajax({
+    url: '/api/v1/users/getLocation',
+    type: 'POST',
+    data: { 
+      lng: -0.076296,
+      lat: 51.523853
+    }
+  })
+  .done(function(data) {
+    data.forEach(function(location) {
 
       var marker = new google.maps.Marker({
-        position: {lat: position.coords.latitude, lng: position.coords.longitude},
+        position: {lat: location.geo[1], lng: location.geo[0]},
         map: map
       });
-    }
-    initMap();
+      attachMarkerMessage(marker, location.name);
+
+      console.log(location);
+      var content = $(`<a href="">${location.name}</a>`)
+      var div = $('<div>').append(content)
+      var li = $('<li>').append(div)
+
+      $('.places-ul').append(li)
+    })
+  })
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 51.523853, lng: -0.076296},
+      zoom: 14
+    });
+
+    var marker = new google.maps.Marker({
+      position: {lat: 51.523853, lng: -0.076296},
+      map: map
+    });
   }
+  initMap();
+
 
 
   // Attaches an info window to a marker with the provided message. When the
@@ -60,18 +54,17 @@ $( document ).ready(function(){
     marker.addListener('click', function() {
       infowindow.open(marker.get('map'), marker);
     });
-
   }
 
-  function geo_error() {
-    alert("Sorry, no position available.");
-  }
+  // function geo_error() {
+  //   alert("Sorry, no position available.");
+  // }
 
-  var geo_options = {
-    enableHighAccuracy: true, 
-    maximumAge        : 30000, 
-    timeout           : 27000
-  };
+  // var geo_options = {
+  //   enableHighAccuracy: true, 
+  //   maximumAge        : 30000, 
+  //   timeout           : 27000
+  // };
 
-  navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+  // navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 });
