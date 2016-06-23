47aeff9364fd7fa92699ae3055db999541be63c9
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-06-23 12:56:47 +0200

    local-passport

--- passport/local.js
+++ passport/local.js
@@ -11,7 +11,30 @@ module.exports = function(passport) {
   },
   function(req, username, password, done) {
     process.nextTick(function() {
-
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({ 'username' :  username }, function(err, user) {
+          // if there are any errors, return the error
+          if (err) {
+            return done(err);
+          }
+          // check to see if theres already a user with that email
+          if (user) {
+            return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+          } else {
+            // if there is no user with that email
+            // create the user
+            var user = new User();
+            user.username = username;
+            user.password = user.generateHash(password);
+            user.save(function(err) {
+              if (err) {
+                throw err;
+              }
+              return done(null, user);
+            });
+          }
+      });
     });
   }));
 };
